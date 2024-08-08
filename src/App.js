0129f21ef63b728dc7a4891a6f7e5b55cfd654d4
dhairyshil24 <dhairyshil87@gmail.com>
2024-08-08 13:01:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Window Resizer</h1>
+      <h3>Put the value below</h3>
+
+      <input type='text'></input>
+
+    </div>
+  );
+}
+
+export default App;
